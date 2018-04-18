dd035ab911635ffb97a3f59cf7ff32be1efd3392
Belciug Ovidiu-Mihai <ovidiumihaibelciug@gmail.com>
2018-04-18 18:07:22 +0300

    Scss, font-awesome and material setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
