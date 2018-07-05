6f9e6d465137b07211e9b5ac1a4fb6ef0a61e8d6
Belciug Ovidiu-Mihai <ovidiumihaibelciug@gmail.com>
2018-07-05 22:30:36 +0300

    Updates

--- src/apollo.js
+++
@@ -1,5 +0,0 @@
-import ApolloClient from "apollo-boost";
-
-export const client = new ApolloClient({
-  uri: "http://fiipractic-react.cultofcoders.com/graphql"
-});
--- src/stores/index.js
+++
@@ -1,9 +0,0 @@
-import UserStore from "./UserStore";
-import PostStore from "./PostStore";
-
-export default class RootStore {
-  constructor() {
-    this.userStore = new UserStore(this);
-    this.postStore = new PostStore(this);
-  }
-}
